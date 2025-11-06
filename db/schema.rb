dbf592778c1ee60ad929bdd8fa7f002710c5ad9a
jlm <j.l.messenger@ieee.org>
2025-11-06 22:36:41 +0000

    Create minst model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_02_181556) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_06_222732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
+  create_table "minsts", force: :cascade do |t|
+    t.string "code"
     t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_minsts_on_code"
+    t.index ["name"], name: "index_minsts_on_name"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end

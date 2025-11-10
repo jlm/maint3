0f3cb5f3893c4580571171d984b8b176d6697855
jlm <j.l.messenger@ieee.org>
2025-11-10 23:34:25 +0000

    Validation and scope for dates in Minute.

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -2,4 +2,6 @@ class Minute < ApplicationRecord
   belongs_to :item
   belongs_to :meeting
   belongs_to :minst
+  validates :date, presence: true, unless: -> { Maint3::Config::Importing }
+  scope :date_valid, -> { where.not(date: nil) }
 end
